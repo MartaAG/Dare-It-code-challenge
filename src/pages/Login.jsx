a8663ccd2440a626d0ae1157da820b971fad4388
Marta_AG <marta.guszek@gmail.com>
2022-01-12 17:06:41 +0100

    fix remember me option

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,21 +2,27 @@ import React, { useEffect, useState } from 'react';
 import Header from '../components/atom/header';
 import Form from '../components/organism/Form';
 
+const LOGIN_KEY = 'loginData';
+
 const Login = () => {
     const [initialData, setInitialData] = useState(undefined);
     const [rememberMe, setRememberMe] = useState(true);
    
     useEffect(() => {
-        const savedData = localStorage.getItem('loginData');
+        const savedData = localStorage.getItem(LOGIN_KEY);
         if(savedData){
             setInitialData(JSON.parse(savedData))
-        }
+        } 
+
     }, [])
 
     const onSubmit = (loginData, rememberMe) => {
         if(rememberMe) {
-            localStorage.setItem('loginData', JSON.stringify(loginData));
+            localStorage.setItem(LOGIN_KEY, JSON.stringify(loginData));
         }
+         else {
+             localStorage.removeItem(LOGIN_KEY)
+         }
         console.log('send to api', loginData)
     }
 
